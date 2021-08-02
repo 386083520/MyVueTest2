19508b5bcc69c315b369ee6667da880ed93cbe5d
386083520 <386083520@qq.com>
2021-08-02 17:06:16 +0800

    Observer类

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -1,7 +1,22 @@
+import Dep from "./dep";
 import { isObject, isPlainObject } from "../util/index";
+import { def } from '../util/index'
 
 export class Observer {
     constructor (value) {
+        this.value = value
+        this.dep = new Dep()
+        this.vmCount = 0
+        def(value, '__ob__', this)
+        if (Array.isArray(value)) {
+            this.observeArray(value)
+        } else {
+            this.walk(value)
+        }
+    }
+    walk (obj) {
+    }
+    observeArray (items) {
     }
 }
 
--- src/core/util/index.js
+++ src/core/util/index.js
@@ -1,3 +1,4 @@
 export * from '../../shared/util'
+export * from './lang'
 export * from './options'
 export * from './env'
