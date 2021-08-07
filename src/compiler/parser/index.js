4c5252a1b9e3e86ebf186e4e26eafb27bc935733
386083520 <386083520@qq.com>
2021-08-08 07:46:52 +0800

    parseHTML start方法完善

--- src/compiler/parser/index.js
+++ src/compiler/parser/index.js
@@ -6,6 +6,8 @@ import {no} from "../../shared/util";
 import { isIE, isEdge, isServerRendering } from "../../core/util/index";
 import { getAndRemoveAttr } from "../helpers";
 
+const invalidAttributeRE = /[\s"'<>\/=]/
+
 export let warn
 
 let transforms
@@ -75,7 +77,7 @@ export function parse (template, options) {
     }
 
 
-    function closeElement (element) {// 关闭element
+    function closeElement (element) {// 关闭element // TODO
         if (!inVPre && !element.processed) {
             element = processElement(element, options)
         }
@@ -86,7 +88,20 @@ export function parse (template, options) {
     }
 
     function checkRootConstraints (el) { // 校验检查，不要用slot、template做根节点，也不要用 v-for 属性，因为这些都可能产生多个根节点
-
+        if (el.tag === 'slot' || el.tag === 'template') {
+            warnOnce(
+                `Cannot use <${el.tag}> as component root element because it may ` +
+                'contain multiple nodes.',
+                { start: el.start }
+            )
+        }
+        if (el.attrsMap.hasOwnProperty('v-for')) {
+            warnOnce(
+                'Cannot use v-for on stateful component root element because ' +
+                'it renders multiple elements.',
+                el.rawAttrsMap['v-for']
+            )
+        }
     }
     parseHTML(template, {
         warn, // 警告函数
@@ -107,7 +122,23 @@ export function parse (template, options) {
                 element.ns = ns
             }
             if (true) { // TODO
-
+                if (options.outputSourceRange) {
+                    element.start = start
+                    element.end = end
+                    element.rawAttrsMap = [] // TODO
+                }
+                attrs.forEach(attr => {
+                    if (invalidAttributeRE.test(attr.name)) {
+                        warn(
+                            `Invalid dynamic argument expression: attribute names cannot contain ` +
+                            `spaces, quotes, <, >, / or =.`,
+                            {
+                                start: attr.start + attr.name.indexOf(`[`),
+                                end: attr.start + attr.name.length
+                            }
+                        )
+                    }
+                })
             }
             if (isForbiddenTag(element) && !isServerRendering()) { // 判断生成的element是否是禁止标签
                 element.forbidden = true
@@ -128,12 +159,27 @@ export function parse (template, options) {
                     inVPre = true
                 }
             }
+            if (platformIsPreTag(element.tag)) { // pre 元素可定义预格式化的文本。被包围在 pre 元素中的文本通常会保留空格和换行符
+                inPre = true
+            }
+            if (inVPre) {
+                processRawAttrs(element)
+            }else if(!element.processed){ // 没有v-pre 并且没有processed
+                processFor(element) // v-for
+                processIf(element) // v-if
+                processOnce(element) // v-once
+            }
             if (!root) {
                 root = element
+                if (true) { // TODO
+                    checkRootConstraints(root)
+                }
             }
             if (!unary) {
                 currentParent = element
                 stack.push(element)
+            }else{
+                closeElement(element)
             }
         },
         end (tag, start, end) {
@@ -204,4 +250,76 @@ function processPre (el) {
     if (getAndRemoveAttr(el, 'v-pre') != null) {
         el.pre = true
     }
+}
+
+function processRawAttrs (el) { // 存在v-pre的时候，遍历当前所有的attrsList，依次保存到e.attrs上面
+    const list = el.attrsList
+    const len = list.length
+    if (len) {
+        const attrs = el.attrs = new Array(len)
+        for (let i = 0; i < len; i++) {
+            attrs[i] = {
+                name: list[i].name,
+                value: JSON.stringify(list[i].value)
+            }
+            if (list[i].start != null) {
+                attrs[i].start = list[i].start
+                attrs[i].end = list[i].end
+            }
+        }
+    } else if (!el.pre) { // TODO
+
+    }
+}
+
+export function processFor (el) { // 处理v-for
+    let exp
+    if ((exp = getAndRemoveAttr(el, 'v-for'))) {
+        const res = parseFor(exp)
+        if (res) {
+
+        }else{ // TODO
+            warn(
+                `Invalid v-for expression: ${exp}`,
+                el.rawAttrsMap['v-for']
+            )
+        }
+    }
+}
+
+function processIf (el) {
+    const exp = getAndRemoveAttr(el, 'v-if')
+    if (exp) {
+        el.if = exp
+        addIfCondition(el, {
+            exp: exp,
+            block: el
+        })
+    } else {
+        if (getAndRemoveAttr(el, 'v-else') != null) {
+            el.else = true
+        }
+        const elseif = getAndRemoveAttr(el, 'v-else-if')
+        if (elseif) {
+            el.elseif = elseif
+        }
+    }
+}
+
+function processOnce (el) {
+    const once = getAndRemoveAttr(el, 'v-once')
+    if (once != null) {
+        el.once = true //子组件中分别加入v-once，当每次切换组件效果时，不再需要每次都经过 创建——销毁 的过程，而是在内存中直接取用上一次使用过的组件的内容，可有效提高静态内容的展示效率
+    }
+}
+
+export function parseFor (exp) {
+    return exp
+}
+
+export function addIfCondition (el, condition) {
+    if (!el.ifConditions) {
+        el.ifConditions = []
+    }
+    el.ifConditions.push(condition)
 }
